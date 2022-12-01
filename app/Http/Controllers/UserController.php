ce2d90e5f7da63063ded2bf3acfc9a192b87c47a
Dreeshyaa <60399318+Dreeshyaa@users.noreply.github.com>
2022-12-01 22:15:47 +0545

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(User::latest()->get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=> bcrypt($request->password)
+        ]);
+        return response()->json('successfully created');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return response()->json(User::whereId($id)->first());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::whereId($id)->first();
+
+        $user->update([
+            'name'=>$request->name,
+            'email'=>$request->email,
+        ]);
+        return response()->json('success');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::whereId($id)->first()->delete();
+        return response()->json("success");
+    }
+    
+    public function register(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email',
+            'password'=>'required|confirmed',
+            'tc'=>'required',
+        ]);
+        if(User::where('email', $request->email)->first()){
+            return response([
+                'message' => 'Email already exists',
+                'status'=>'failed'
+            ], 200);
+        }
+
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password),
+            'tc'=>json_decode($request->tc),
+        ]);
+        $token = $user->createToken($request->email)->plainTextToken;
+        return response([
+            'token'=>$token,
+            'message' => 'Registration Success',
+            'status'=>'success'
+        ], 201);
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if($user && Hash::check($request->password, $user->password)){
+            $token = $user->createToken($request->email)->plainTextToken;
+            return response([
+                'token'=>$token,
+                'message' => 'Login Success',
+                'status'=>'success'
+            ], 200);
+        }
+        return response([
+            'message' => 'The Provided Credentials are incorrect',
+            'status'=>'failed'
+        ], 401);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response([
+            'message' => 'Logout Success',
+            'status'=>'success'
+        ], 200);
+    }
+    
+    public function logged_user(){
+        $loggeduser = auth()->user();
+        return response([
+            'user'=>$loggeduser,
+            'message' => 'Logged User Data',
+            'status'=>'success'
+        ], 200);
+    }
+
+    public function change_password(Request $request){
+        $request->validate([
+            'password' => 'required|confirmed',
+        ]);
+        $loggeduser = auth()->user();
+        $loggeduser->password = Hash::make($request->password);
+        $loggeduser->save();
+        return response([
+            'message' => 'Password Changed Successfully',
+            'status'=>'success'
+        ], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::resource('users',UserController::class);
+//public routes
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/send-reset-password-email', [PasswordResetController::class, 'send_reset_password_email']);
+Route::post('/reset-password/{token}', [PasswordResetController::class, 'reset']);
+
+// Protected Routes
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::get('/loggeduser', [UserController::class, 'logged_user']);
+    Route::post('/changepassword', [UserController::class, 'change_password']);
+});
